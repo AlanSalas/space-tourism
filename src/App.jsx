1a61f7257f1308727e60a6526bb24e466b257335
AlanSalas <dubekone@gmail.com>
2021-11-28 05:31:29 -0500

    Project setup * add assets * dependencies * routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "layout";
+import { Home, Destination, Crew, Technology } from "pages";
 
 const App = () => {
   return (
-    <div>
-      <h1>App :)</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="destination" element={<Destination />} />
+          <Route path="destination" element={<Destination />} />
+          <Route path="crew" element={<Crew />} />
+          <Route path="technology" element={<Technology />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 };
 
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from "pages/Home";
+import Destination from "pages/Destination";
+import Crew from "pages/Crew";
+import Technology from "pages/Technology";
+
+export { Home, Destination, Crew, Technology };
